fafa149da423a2d1242f6faa4be3ec665c335c44
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-08-16 23:28:07 -0300

    finalizando css e iniciando js com seleção de elementos e eventos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+// selection elements
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+// function
 
+//events
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("enviou um form");
+});
